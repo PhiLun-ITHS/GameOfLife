200d29d8043e8574b067cadc2f159ad011c614e6
PL <philip.lundin@iths.se>
2022-01-10 15:26:47 +0100

    added int neighbor and logic rules

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -2,12 +2,27 @@
 public class Cell {
 
     private boolean CellAlive;
+    private int neighbors;
 
     public boolean isCellAlive() {
+
+        if (neighbors < 2) {
+            CellAlive = false;
+        } else if (neighbors == 2) {
+            CellAlive = true;
+        } else if (neighbors == 3) {
+            CellAlive = true;
+        } else {
+            CellAlive = false;
+        }
         return CellAlive;
     }
 
     public void setCellAlive(boolean cellAlive) {
         CellAlive = cellAlive;
+    }
+
+    public void setNeighbors(int neighbors) {
+        this.neighbors = neighbors;
     }
 }
