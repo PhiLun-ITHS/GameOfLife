c6792697b55b5190508dbfe4aa0e75f44d89091e
PL <philip.lundin@iths.se>
2022-01-10 15:22:35 +0100

    added multiple tests

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -6,27 +6,68 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class CellTest {
 
     @Test
-    public void cellShouldDieWithZeroNeighbors(){
+    public void CellShouldDieWithoutNeighbor(){
 
         Cell cell = new Cell();
+        cell.setNeighbors(0);
 
-        assertFalse(cell.isCellAlive());
-    }
-
-
-
-
-
-
-
-
-    @Test
-    public void cellShouldDieWithOneNeighbor(){
-
+        assertEquals(false, cell.isCellAlive());
     }
 
     @Test
-    public void cellShouldDieWithMoreThanThreeNeighbors(){
+    public void CellShouldDieWithOneNeighbor(){
 
+        Cell cell = new Cell();
+        cell.setNeighbors(1);
+
+        assertEquals(false, cell.isCellAlive());
     }
+
+    @Test
+    public void CellShouldDieWithMoreThanThreeNeighbors(){
+
+        Cell cell = new Cell();
+        cell.setNeighbors(4);
+
+        assertEquals(false, cell.isCellAlive());
+    }
+
+    @Test
+    public void CellShouldStayAliveWithTwoAliveNeighbors(){
+
+        Cell cell = new Cell();
+        cell.setNeighbors(2);
+
+        assertEquals(true, cell.isCellAlive());
+    }
+
+    @Test
+    public void CellShouldStayAliveWithThreeAliveNeighbors(){
+
+        Cell cell = new Cell();
+        cell.setNeighbors(3);
+
+        assertEquals(true, cell.isCellAlive());
+    }
+
+    @Test
+    public void DeadCellShouldBeAliveWithThreeAliveNeighbors(){
+
+        Cell cell = new Cell();
+        cell.setNeighbors(3);
+
+        assertEquals(true, cell.isCellAlive());
+    }
+
+
+/*
+    @Test
+    public void CellShouldDieOnEdges(){
+        Cell cell = new Cell();
+
+        cell.setCellAlive(false);
+
+        assertEquals(false, cell.isCellAlive());
+    }
+ */
 }
