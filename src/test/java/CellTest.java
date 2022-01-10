280fd71e5267ffae517c1365484034b7ee7e44e2
PL <philip.lundin@iths.se>
2022-01-10 14:05:14 +0100

    setting up tests and class

---
+++ src/test/java/CellTest.java
@@ -0,0 +1,29 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CellTest {
+
+    @Test
+    public void cellShouldDieWithZeroNeighbors(){
+
+        Cell cell = new Cell();
+    }
+
+
+
+
+
+
+
+
+    @Test
+    public void cellShouldDieWithOneNeighbor(){
+
+    }
+
+    @Test
+    public void cellShouldDieWithMoreThanThreeNeighbors(){
+
+    }
+}
