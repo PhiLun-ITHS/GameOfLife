792d85addd3c06f04cbe8f2258e4fca922ee08ca
PL <philip.lundin@iths.se>
2022-01-11 11:52:43 +0100

    added boolean grid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int rows = 4;
-        int arrayLength = 8;
+        int M = 8, N = 8;
 
-        for (int i = 0; i < rows; i++) {
-            int[] grid = new int[arrayLength];
+        boolean[][] grid = {
+                { false, false, false, false, false, false, false, false },
+                { false, false, false, false, false, false, false, false },
+                { false, false, false, false, false, false, false, false },
+                { false, false, false, true, true, false, false, false },
+                { false, false, true, true, false, false, false, false },
+                { false, false, false, false, false, true, false, false },
+                { false, false, false, false, true, false, false, false },
+                { false, false, false, false, false, false, false, false }
+        };
+
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (grid[i][j] == false)
+                    System.out.print(".");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
         }
+        System.out.println();
+        //nextGeneration(grid, M, N);
     }
 }
