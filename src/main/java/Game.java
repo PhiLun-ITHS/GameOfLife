68a143b2b4f8cfca0eb4eb3f38fd6c99944dbe25
PL <philip.lundin@iths.se>
2022-01-12 17:38:14 +0100

    added board

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,24 @@
 
 public class Game {
 
-    int width = 8, height = 8;
+    public static void main(String[] args) {
 
-    int[][] board = new int[width][height];
+        Game game = new Game();
+        game.createBoard();
+    }
 
+    private void createBoard(){
 
+        int width = 8, height = 8;
+
+        Cell cell = new Cell();
+
+        int[][] board = new int[width][height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                board[i][j] = cell.DEAD;
+            }
+        }
+    }
 }
